c09ecf58bad21d2b1354b20ae95d1b7bc2178a6a
Tim Goll <tim@timgoll.de>
2019-10-07 23:26:59 +0200

    small fix

--- lua/terrortown/entities/roles/sniffer/shared.lua
+++ lua/terrortown/entities/roles/sniffer/shared.lua
@@ -58,11 +58,11 @@ end
 if SERVER then
 	-- Give Loadout on respawn and rolechange	
 	function ROLE:GiveRoleLoadout(ply, isRoleChange)
-		ply:GiveEquipmentWeapon('weapon_ttt2_lens')
+		ply:GiveEquipmentWeapon("weapon_ttt2_lens")
 	end
 
 	-- Remove Loadout on death and rolechange
 	function ROLE:RemoveRoleLoadout(ply, isRoleChange)
-		ply:StripWeapon('weapon_ttt2_lens')
+		ply:StripWeapon("weapon_ttt2_lens")
 	end
 end
