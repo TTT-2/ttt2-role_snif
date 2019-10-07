de4186532c68e7ea57a3cb7fa5f6970959f6fc1e
Alf Müller <4lf-mueller@gmx.de>
2019-10-07 23:17:49 +0200

    Update for TTT2 v0.5.7b

--- lua/terrortown/entities/roles/sniffer/shared.lua
+++ lua/terrortown/entities/roles/sniffer/shared.lua
@@ -3,64 +3,63 @@ if SERVER then
 	resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_snif.vmt")
 end
 
-ROLE.Base = "ttt_role_base"
+function ROLE:PreInitialize()
+	self.color = Color(81, 123, 226, 255)
+	self.dkcolor = Color(17, 69, 200, 255)
+	self.bgcolor = Color(255, 157, 72, 255)
+	self.abbr = "snif"
+	self.scoreKillsMultiplier = 1
+	self.scoreTeamKillsMultiplier = -8
+	self.fallbackTable = {}
+	self.unknownTeam = true
+	
+	self.defaultTeam = TEAM_INNOCENT
+	self.defaultEquipment = SPECIAL_EQUIPMENT
 
-ROLE.color = Color(81, 123, 226, 255)
-ROLE.dkcolor = Color(17, 69, 200, 255)
-ROLE.bgcolor = Color(255, 157, 72, 255)
-ROLE.abbr = "snif"
-ROLE.defaultTeam = TEAM_INNOCENT
-ROLE.defaultEquipment = SPECIAL_EQUIPMENT
-ROLE.scoreKillsMultiplier = 1
-ROLE.scoreTeamKillsMultiplier = -8
-ROLE.fallbackTable = {}
-ROLE.unknownTeam = true
+	-- conVarData
+	self.conVarData = {
+		pct = 0.13,
+		maximum = 32,
+		minPlayers = 8,
+		minKarma = 600,
 
--- conVarData
-ROLE.conVarData = {
-	pct = 0.13,
-	maximum = 32,
-	minPlayers = 8,
-	minKarma = 600,
+		credits = 1,
+		creditsTraitorKill = 0,
+		creditsTraitorDead = 1,
 
-	credits = 1,
-	creditsTraitorKill = 0,
-	creditsTraitorDead = 1,
+		togglable = true,
+		shopFallback = SHOP_FALLBACK_DETECTIVE
+	}
+end
 
-	togglable = true,
-	shopFallback = SHOP_FALLBACK_DETECTIVE
-}
-
--- now link this subrole with its baserole
-hook.Add("TTT2BaseRoleInit", "TTT2ConBRDWithSnif", function()
-	SNIFFER:SetBaseRole(ROLE_DETECTIVE)
-end)
-
-if CLIENT then
-	-- if sync of roles has finished
-	hook.Add("TTT2FinishedLoading", "SnifferInitD", function()
+function ROLE:Initialize()
+	roles.SetBaseRole(self, ROLE_DETECTIVE)
+	
+	if CLIENT then
 		-- setup here is not necessary but if you want to access the role data, you need to start here
 		-- setup basic translation !
-		LANG.AddToLanguage("English", SNIFFER.name, "Sniffer")
-		LANG.AddToLanguage("English", "info_popup_" .. SNIFFER.name, [[You are a Sniffer!
+		LANG.AddToLanguage("English", self.name, "Sniffer")
+		LANG.AddToLanguage("English", "info_popup_" .. self.name, [[You are a Sniffer!
 	Try to get some credits!]])
-		LANG.AddToLanguage("English", "body_found_" .. SNIFFER.abbr, "This was a Sniffer...")
-		LANG.AddToLanguage("English", "search_role_" .. SNIFFER.abbr, "This person was a Sniffer!")
-		LANG.AddToLanguage("English", "target_" .. SNIFFER.name, "Sniffer")
-		LANG.AddToLanguage("English", "ttt2_desc_" .. SNIFFER.name, [[The Sniffer is a Detective (who works together with the other detectives)]])
+		LANG.AddToLanguage("English", "body_found_" .. self.abbr, "This was a Sniffer...")
+		LANG.AddToLanguage("English", "search_role_" .. self.abbr, "This person was a Sniffer!")
+		LANG.AddToLanguage("English", "target_" .. self.name, "Sniffer")
+		LANG.AddToLanguage("English", "ttt2_desc_" .. self.name, [[The Sniffer is a Detective (who works together with the other detectives)]])
 
 		---------------------------------
 
 		-- maybe this language as well...
-		LANG.AddToLanguage("Deutsch", SNIFFER.name, "Sniffer")
-		LANG.AddToLanguage("Deutsch", "info_popup_" .. SNIFFER.name, [[Du bist ein Sniffer!
+		LANG.AddToLanguage("Deutsch", self.name, "Sniffer")
+		LANG.AddToLanguage("Deutsch", "info_popup_" .. self.name, [[Du bist ein Sniffer!
 	Versuche ein paar Credits zu bekommen!]])
-		LANG.AddToLanguage("Deutsch", "body_found_" .. SNIFFER.abbr, "Er war ein Sniffer...")
-		LANG.AddToLanguage("Deutsch", "search_role_" .. SNIFFER.abbr, "Diese Person war ein Sniffer!")
-		LANG.AddToLanguage("Deutsch", "target_" .. SNIFFER.name, "Sniffer")
-		LANG.AddToLanguage("Deutsch", "ttt2_desc_" .. SNIFFER.name, [[Der Sniffer ist ein Detektiv (der mit den anderen Detektiv-Rollen zusammenarbeitet)]])
-	end)
-else -- SERVER
+		LANG.AddToLanguage("Deutsch", "body_found_" .. self.abbr, "Er war ein Sniffer...")
+		LANG.AddToLanguage("Deutsch", "search_role_" .. self.abbr, "Diese Person war ein Sniffer!")
+		LANG.AddToLanguage("Deutsch", "target_" .. self.name, "Sniffer")
+		LANG.AddToLanguage("Deutsch", "ttt2_desc_" .. self.name, [[Der Sniffer ist ein Detektiv (der mit den anderen Detektiv-Rollen zusammenarbeitet)]])
+	end
+end)
+
+if SERVER then
 	hook.Add("TTT2UpdateSubrole", "TTT2SnifGiveLens", function(ply, old, new)
 		if new == ROLE_SNIFFER then
 			ply:GiveEquipmentWeapon("weapon_ttt2_lens")
