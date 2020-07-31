95ad8c15936abbd9f5ad22bcc496c7d9e622d138
Tekiad <tekiadp@gmail.cm>
2020-07-31 03:16:28 -0300

    Update sniffer.lua

--- lua/lang/spanish/sniffer.lua
+++ lua/lang/spanish/sniffer.lua
@@ -3,7 +3,7 @@ L = LANG.GetLanguageTableReference("spanish")
 -- GENERAL ROLE LANGUAGE STRINGS
 L[SNIFFER.name] = "Rastreador"
 L["info_popup_" .. SNIFFER.name] = [[¡Eres un Rastreador!
-¡Intenta ganar algunos créditos	!]]
+¡Intenta ganar algunos créditos!]]
 L["body_found_" .. SNIFFER.abbr] = "¡Era un Rastreador!"
 L["search_role_" .. SNIFFER.abbr] = "Esta persona era un Rastreador."
 L["target_" .. SNIFFER.name] = "Rastreador"
