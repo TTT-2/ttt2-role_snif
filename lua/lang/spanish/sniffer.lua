a7e492de02d1c06b0b0fcefa2bf96378883f1600
Tekiad <tekiadp@gmail.cm>
2020-07-31 03:15:12 -0300

    Create sniffer.lua
    sniffer spanish translation

---
+++ lua/lang/spanish/sniffer.lua
@@ -0,0 +1,11 @@
+L = LANG.GetLanguageTableReference("spanish")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SNIFFER.name] = "Rastreador"
+L["info_popup_" .. SNIFFER.name] = [[¡Eres un Rastreador!
+¡Intenta ganar algunos créditos	!]]
+L["body_found_" .. SNIFFER.abbr] = "¡Era un Rastreador!"
+L["search_role_" .. SNIFFER.abbr] = "Esta persona era un Rastreador."
+L["target_" .. SNIFFER.name] = "Rastreador"
+L["ttt2_desc_" .. SNIFFER.name] = [[El Rastreador es un Detective que analiza las huellas y la sangre en estas. 
+Coopera con los inocentes para salir victoriosos.]]
