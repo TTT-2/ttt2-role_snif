fa691a5e23096f7da978ac5f695be46bc100d643
Blueberryy <36592509+Blueberryy@users.noreply.github.com>
2020-10-06 17:09:49 +0500

    Create sniffer.lua (#6)

---
+++ lua/terrortown/lang/russian/sniffer.lua
@@ -0,0 +1,10 @@
+local L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SNIFFER.name] = "Нюхач"
+L["info_popup_" .. SNIFFER.name] = [[Вы нюхач!
+Попытайтесь заработать кредиты!]]
+L["body_found_" .. SNIFFER.abbr] = "Он был нюхачом."
+L["search_role_" .. SNIFFER.abbr] = "Этот человек был нюхачом!"
+L["target_" .. SNIFFER.name] = "Нюхач"
+L["ttt2_desc_" .. SNIFFER.name] = [[Нюхач - детектив, работающий вместе с другими детективами]]
