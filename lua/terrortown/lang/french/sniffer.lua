b0b54d29ebffe14473058e7c65d4d16a8109ac33
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-29 14:57:38 +0100

    Create sniffer.lua
    french translation :Sniffer

---
+++ lua/terrortown/lang/french/sniffer.lua
@@ -0,0 +1,10 @@
+local L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SNIFFER.name] = "Flaireur"
+L["info_popup_" .. SNIFFER.name] = [[Vous êtes un Flaireur!
+Essayez d'obtenir des crédits!]]
+L["body_found_" .. SNIFFER.abbr] = "C'était un Flaireur."
+L["search_role_" .. SNIFFER.abbr] = "Cette personne était un Flaireur!"
+L["target_" .. SNIFFER.name] = "Flaireur"
+L["ttt2_desc_" .. SNIFFER.name] = [[Le Flaireur est un détective qui collabore avec les autres détectives]]
